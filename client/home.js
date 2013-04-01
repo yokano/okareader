4ff8d4498ff7943aadccf501b26bf5cf3cf15c15
Yuta Okano <yuta.okano@gmail.com>
2013-04-01 13:21:38 +0900

    編集モード

--- client/home.js
+++ client/home.js
@@ -12,6 +12,8 @@ $('.folder_page').live('pageinit', function() {
 	var addFeedButton = $(this).find('#add_feed_button');
 	var feedURL = $(this).find('#feed_url');
 	var addFeed = $(this).find('#add_feed');
+	var editButton = $(this).find('#edit');
+	var editMode = false;
 	
 	// フォルダを追加するボタン
 	addFolderButton.bind('tap', function() {
@@ -58,5 +60,45 @@ $('.folder_page').live('pageinit', function() {
 				console.log('error');
 			}
 		});
+	});
+	
+	// 編集ボタン
+	editButton.bind('tap', function() {
+		if(editMode) {
+			editMode = false;
+			$(this).find('.ui-btn-text').html('編集');
+			$(this).find('.ui-icon').removeClass('ui-icon-check').addClass('ui-icon-edit');
+			
+			// リンクを有効化
+			contents.find('a').unbind('tap');
+			
+			// アイコンを戻す
+			contents.find('.ui-icon').addClass('ui-icon-arrow-r').removeClass('ui-icon-gear');
+			
+			// メッセージ非表示
+			$('#edit_message').remove();
+			
+		} else {
+			editMode = true;
+			$(this).find('.ui-btn-text').html('完了');
+			$(this).find('.ui-icon').removeClass('ui-icon-edit').addClass('ui-icon-check');
+	
+			// リンクを無効化
+			$.each(contents.children(), function(i, data) {
+				$(data).find('a').bind('tap', function() {
+					return false;
+				});
+			});
+			
+			// アイコンを変更
+			$.each(contents.children(), function(i, data) {
+				$(data).find('.ui-icon').removeClass('ui-icon-arrow-r').addClass('ui-icon-gear');
+			});
+			
+			// メッセージ表示
+			contents.prepend($('<li id="edit_message" data-role="list-divider">編集したいタイトルをタップ</li>'));
+			
+			contents.listview('refresh');
+		}
 	});
 });
